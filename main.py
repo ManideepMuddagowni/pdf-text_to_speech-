f4682e4ff09e0190bfdf97f886b27b0ac9a5d392
Manideep Goud Muddagowni <66520928+ManideepMuddagowni@users.noreply.github.com>
2021-02-21 14:57:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import PyPDF2
 from gtts import gTTS
 import os
 from os.path import splitext
+### book name is ML.pdf
 mytext = open('ML.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(mytext)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
+### speech will be starting from starting from 26th page
 for num in range(26, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
